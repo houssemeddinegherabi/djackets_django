7834abef8134397046f73dcbfcd6d229f1383aa2
houssemeddinegherabi <83501913+houssemeddinegherabi@users.noreply.github.com>
2021-07-28 01:06:05 +0100

    Create settings.py

--- djackets_django/settings.py
+++ djackets_django/settings.py
@@ -127,7 +127,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
